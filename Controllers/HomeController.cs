1f4c400ad405799fe3912ec12153b115a0cdebb0
Nick <ntambroson@dmacc.edu>
2021-02-03 21:21:08 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppAmbroson.Models;
+
+namespace FirstResponsiveWebAppAmbroson.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "";
+            ViewBag.Age = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FirstResponsiveWebAppModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = model.Name;
+                ViewBag.Age = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Name = "";
+                ViewBag.Age = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/FirstResponsiveWebAppModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppAmbroson.Models
+{
+    public class FirstResponsiveWebAppModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        [RegularExpression("^[A-Za-z](([A-Za-z])|([A-Za-z]['-](?=[A-Za-z]))|( (?=[A-Za-z])))*$", ErrorMessage = "Name must only contain letters, hyphens, and apostrophes.")]
+        public string Name { get; set; }
+
+        
+        [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+        public class YearRange : RangeAttribute
+        {
+            public YearRange(int minimum) : base(minimum, DateTime.Now.Year)
+            {
+            }
+        }
+        
+        [YearRange(0, ErrorMessage = "Please enter a valid birth year")]
+        [Required(ErrorMessage = "Please enter your birth year")]
+        public int BirthYear { get; set; }
+
+        public int AgeThisYear()
+        {
+            int birthYear = BirthYear;
+            DateTime currentYear = DateTime.Today;
+            int currentAge;
+
+            currentAge = currentYear.Year - birthYear;
+
+            return currentAge;
+        }
+    }
+}
